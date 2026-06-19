7fe44156edf372c6738afd180f297a5a5dbde535
DLuiso <luis@aviditytechnologies.com>
2026-06-18 21:07:19 -0500

    Fix system health auth dependency

--- apps/api/src/modules/system-health/system-health.module.ts
+++ apps/api/src/modules/system-health/system-health.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { SystemHealthController } from "./system-health.controller";
 import { SystemHealthService } from "./system-health.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [SystemHealthController],
   providers: [SystemHealthService]
 })
