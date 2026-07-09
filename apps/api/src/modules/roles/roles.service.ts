8e0781def4c51700c895a02ee1c3817577de980f
Luis Mena <lsonycat@gmail.com>
2026-07-09 11:41:38 -0500

    Accept deterministic permission UUIDs

--- apps/api/src/modules/roles/roles.service.ts
+++ apps/api/src/modules/roles/roles.service.ts
@@ -214,7 +214,7 @@ export class RolesService {
   }
 
   private isUuid(value: string) {
-    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value);
   }
 
   private isKnownPermissionName(value: string) {
